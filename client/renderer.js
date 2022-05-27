15a3953aa677b14b7c77210c2619171950b79313
tavichh <tavichh@rustysheep.com>
2022-05-27 14:26:44 -0400

    added a way to measure tries

--- client/renderer.js
+++ client/renderer.js
@@ -1,13 +1,20 @@
+/* It's creating an array of programs that the user can access. */
 let programs = [
   30295, // display delaware easter egg
   42069, // display meme easter egg
 ];
+
+/* Setting the variable `totalTries` to 0. */
+let totalTries = 0;
+/* Setting the maximum number of tries the user has to 3. */
+const maximumTries = 3;
 
 /**
  * It takes the user's input, checks if it's a number, and if it is, it searches the array for the
  * program.
  */
 function getInput() {
+  document.getElementById("input").focus();
   let input = document.getElementById("input").value;
   console.log("the user entered " + input);
   if (isNaN(input)) {
@@ -18,6 +25,19 @@ function getInput() {
     input = parseInt(input);
     searchArray(input);
   }
+  totalTries++;
+  console.log(maximumTries - totalTries + " tries remaining");
+  if (totalTries >= maximumTries) {
+    lockUser();
+  }
+}
+
+/**
+ * If the user enters the wrong password more than three times, the search bar will be hidden.
+ */
+function lockUser() {
+  console.log("too many tries, locking user out of the terminal");
+  document.getElementById("search").style.display = "none";
 }
 /**
  * It searches the array for the input, and if it finds it, it displays the program
@@ -28,9 +48,10 @@ function searchArray(input) {
     if (input == programs[i]) {
       console.log("found the program!");
       displayProgram(input);
+      totalTries = 0;
       break;
     } else {
-      console.log("program not found. resetting...");
+      console.log("not the correct program...searching again...");
       document.getElementById("input").value = "";
     }
   }
@@ -41,6 +62,6 @@ function searchArray(input) {
  * @param input - the input from the user
  */
 function displayProgram(input) {
-    let search = document.getElementById("search");
-    search.style.display="none";
+  let search = document.getElementById("search");
+  search.style.display = "none";
 }
